88352a0cf9e127aa5251e421ebadf8ec1f5e1b54
MosNes <7hekarl@gmail.com>
2022-12-11 10:43:46 -0500

    set up pinia global store, imported store into HomeView.vue and updated values in template

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,46 +1,52 @@
 <template>
   <div class="home">
     <div class="count">
-      {{count}}
+      {{ counterStore.count }}
     </div>
     <div class="buttons">
-      <button @click="decreaseCount">-</button>
-      <button @click="increaseCount">+</button>
+      <button @click="counterStore.decreaseCount">-</button>
+      <button @click="counterStore.increaseCount">+</button>
     </div>
     <hr>
     <div>
-      This counter is: {{oddOrEven}}
+      This counter is: {{counterStore.oddOrEven}}
     </div>
   </div>
 </template>
 
 <script setup>
+//import global store and assign to variable
+//-------------------------------------------------------
+import {useCounterStore} from '@/stores/counter'
+
+const counterStore = useCounterStore();
+
 //Composition API Imports
 //-------------------------------------------------------
-import { ref, computed } from 'vue';
+// import { ref, computed } from 'vue';
 
 //state data variables
 //-------------------------------------------------------
-const count = ref(0);
+// const count = ref(0);
 
 //methods
 //-------------------------------------------------------
-const increaseCount = () => {
-  count.value++;
-};
+// const increaseCount = () => {
+//   count.value++;
+// };
 
-const decreaseCount = () => {
-  count.value--;
-};
+// const decreaseCount = () => {
+//   count.value--;
+// };
 
 //computed properties
 //-------------------------------------------------------
-const oddOrEven = computed( () => {
-  if (count.value % 2 === 0) {
-    return 'even';
-  }
-  return 'odd';
-});
+// const oddOrEven = computed( () => {
+//   if (count.value % 2 === 0) {
+//     return 'even';
+//   }
+//   return 'odd';
+// });
 
 </script>
 
