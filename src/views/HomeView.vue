85657d018aae8dd88307e3e199ff61971aa49f73
MosNes <7hekarl@gmail.com>
2022-12-11 10:20:57 -0500

    added state data, methods, and computed properties to home view

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,56 @@
 <template>
   <div class="home">
-    <h1>Home</h1>
+    <div class="count">
+      {{count}}
+    </div>
+    <div class="buttons">
+      <button @click="decreaseCount">-</button>
+      <button @click="increaseCount">+</button>
+    </div>
+    <hr>
+    <div>
+      This counter is: {{oddOrEven}}
+    </div>
   </div>
 </template>
+
+<script setup>
+//Composition API Imports
+//-------------------------------------------------------
+import { ref, computed } from 'vue';
+
+//state data variables
+//-------------------------------------------------------
+const count = ref(0);
+
+//methods
+//-------------------------------------------------------
+const increaseCount = () => {
+  count.value++;
+};
+
+const decreaseCount = () => {
+  count.value--;
+};
+
+//computed properties
+//-------------------------------------------------------
+const oddOrEven = computed( () => {
+  if (count.value % 2 === 0) {
+    return 'even';
+  }
+  return 'odd';
+});
+
+</script>
+
+<style>
+.count {
+  font-size: 60px;
+  margin: 20px;
+}
+.buttons button {
+  font-size: 40px;
+  margin: 10px;
+}
+</style>
